124ae880b973ff2a2d01e7d62797eba6b63692d3
Ruta <ceidaiter@gmail.com>
2023-09-11 00:03:09 +0300

    Performing EDA

---
+++ EDA.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+flats_df = pd.read_csv("flats.csv")
+flats_df["price_sqm"] = flats_df["price"] / flats_df["area"]
+#print(flats_df["price_sqm"].describe())
+
+# Remove outliers (e.g., values greater than 10000)
+filtered_df = flats_df[flats_df["price_sqm"] <= 10000]
+filtered_df.hist(column="price_sqm", bins=30)
+
+plt.show()
+
+print(filtered_df["price_sqm"].describe())
