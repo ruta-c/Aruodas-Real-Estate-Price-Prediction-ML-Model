a45854cc073b89b7a2fd618deb7b3d9a6dec4f22
Ruta <ceidaiter@gmail.com>
2023-10-06 16:56:32 +0300

    fixed ' and "

--- Aruodas Scrapper.py
+++ Aruodas Scrapper.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse, parse_qs
 import pandas as pd
 
 def get_advert_data(building_type, where):
-    base_url = f"https://www.aruodas.lt/{building_type}/{where}/puslapis/{{}}/?FOrder=AddDate"
+    base_url = f'https://www.aruodas.lt/{building_type}/{where}/puslapis/{{}}/?FOrder=AddDate'
     executable_path = r'C:\Program Files (x86)\chromedriver-win64\chromedriver.exe'
     driver = webdriver.Chrome(executable_path=executable_path) 
     data_list = []
@@ -20,14 +20,14 @@ def get_advert_data(building_type, where):
             # Handle cookies banner
             try:
                 cookie_banner = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.ID, "onetrust-reject-all-handler"))
+                    EC.presence_of_element_located((By.ID, 'onetrust-reject-all-handler'))
                 )
                 cookie_banner.click()
             except:
                 pass
             
             # Find all advertisement links using XPath
-            advert_links = [link.get_attribute("href") for link in driver.find_elements(By.XPATH, "//h3/a[@href]")]
+            advert_links = [link.get_attribute('href') for link in driver.find_elements(By.XPATH, '//h3/a[@href]')]
             
             if not advert_links:
                 break  
@@ -38,30 +38,30 @@ def get_advert_data(building_type, where):
                 
                 data_dict = {}
                 
-                # Find the element with the "price-eur" class
-                price_element = driver.find_element(By.CLASS_NAME, "price-eur")
+                # Find the element with the 'price-eur' class
+                price_element = driver.find_element(By.CLASS_NAME, 'price-eur')
                 price_value = price_element.text
-                data_dict["Price (EUR)"] = price_value
+                data_dict['Price (EUR)'] = price_value
                 
                 # Find the <a> element containing the coordinates (with explicit wait)
                 try:
                     wait = WebDriverWait(driver, 10)
-                    a_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[data-type="map"]')))
-                    href_value = a_element.get_attribute("href")
+                    a_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[data-type='map']')))
+                    href_value = a_element.get_attribute('href')
                     
                     # Parse the query parameters to extract coordinates
                     query_params = parse_qs(urlparse(href_value).query)
-                    coordinates = query_params.get("query", [""])[0].split(",")
+                    coordinates = query_params.get('query', [''])[0].split(',')
                     latitude = coordinates[0]
                     longitude = coordinates[1]
-                    data_dict["Latitude"] = latitude
-                    data_dict["Longitude"] = longitude
+                    data_dict['Latitude'] = latitude
+                    data_dict['Longitude'] = longitude
                 except:
                     pass
                 
                 # Find all <dt> and <dd> elements containing property information
-                dt_elements = driver.find_elements(By.TAG_NAME, "dt")
-                dd_elements = driver.find_elements(By.TAG_NAME, "dd")
+                dt_elements = driver.find_elements(By.TAG_NAME, 'dt')
+                dd_elements = driver.find_elements(By.TAG_NAME, 'dd')
                 
                 for dt, dd in zip(dt_elements, dd_elements):
                     property_name = dt.text
@@ -70,7 +70,7 @@ def get_advert_data(building_type, where):
                 
                 data_list.append(data_dict)
                 
-            print(f"Scraped page {page}")
+            print(f'Scraped page {page}')
             page += 1
     
     finally:
@@ -82,5 +82,5 @@ def get_advert_data(building_type, where):
 
         return df
 
-# Call the function ("butai" for flats, "namai" for houses, "vilniuje" for Vilnius city, "kaune" for Kaunas city)
-flats_df = get_advert_data("butai", "vilniuje")
+# Call the function ('butai' for flats, 'namai' for houses, 'vilniuje' for Vilnius city, 'kaune' for Kaunas city)
+flats_df = get_advert_data('butai', 'vilniuje')
--- EDA.py
+++ EDA.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-flats_df = pd.read_csv("flats.csv")
-flats_df["price_sqm"] = flats_df["price"] / flats_df["area"]
-#print(flats_df["price_sqm"].describe())
+flats_df = pd.read_csv('flats.csv')
+flats_df['price_sqm'] = flats_df['price'] / flats_df['area']
+#print(flats_df['price_sqm'].describe())
 
 # Remove outliers (e.g., values greater than 10000)
-filtered_df = flats_df[flats_df["price_sqm"] <= 10000]
-filtered_df.hist(column="price_sqm", bins=30)
+filtered_df = flats_df[flats_df['price_sqm'] <= 10000]
+filtered_df.hist(column='price_sqm', bins=30)
 
 plt.show()
 
-print(filtered_df["price_sqm"].describe())
+print(filtered_df['price_sqm'].describe())
