fc169558fc89f8a834814968bccb5f61f5270a92
Ruta C <130843221+ruta-c@users.noreply.github.com>
2023-12-15 15:26:06 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import plotly.express as px
+from sqlalchemy import create_engine
 
 class RealEstateModel:
     def __init__(self):
@@ -34,13 +34,15 @@ class RealEstateModel:
                          'tualetas ir vonia atskirai', 'uždaras kiemas',
                          'virtuvė sujungta su kambariu', 'building_age', 'building_age_reno',
                          'distance_to_center']
+        self.exclude = ['area', 'distance_to_center']
+        self.df[self.features_list] = self.df[self.features_list].apply(lambda col: col.astype('int32') if col.name not in self.exclude else col)
         return self.features_list
 
     def _train_model(self):
         X = self.df[self.features_list]
         y = self.df['price_sqm']
         X_train, X_test, y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        self.model = RandomForestRegressor(min_samples_split=2, n_estimators=739, random_state=42)
+        self.model = RandomForestRegressor(max_depth=13, min_samples_split=2, n_estimators=739, random_state=42)
         self.model.fit(X_train, y_train)
         self.y_pred = self.model.predict(X_test)
         return self.model
@@ -62,6 +64,8 @@ class RealEstateModel:
         scatter_fig.update_layout(margin=dict(l=0, r=20, t=15, b=0))
         return scatter_fig
 
+    def get_training_data(self):
+        return self.df[self.features_list], self.df['price_sqm']
+
     def predict_price(self, input_data):
         return self.model.predict(input_data)
-
