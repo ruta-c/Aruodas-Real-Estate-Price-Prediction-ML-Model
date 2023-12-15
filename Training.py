07752f12e2e7f221eada18b5ea4a2409aadba547
Ruta C <130843221+ruta-c@users.noreply.github.com>
2023-12-15 15:28:57 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -2,8 +2,9 @@ from Model import RealEstateModel
 import joblib
 
 model = RealEstateModel()
+X_train, y_train = model.get_training_data()
+joblib.dump((X_train, y_train), 'training_data.joblib')
 scatter_plot = model.get_scatter_plot()
-
 scatter_json = scatter_plot.to_json()
 
 # Use joblib for model serialization
