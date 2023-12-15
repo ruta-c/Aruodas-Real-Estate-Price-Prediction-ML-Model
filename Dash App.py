bd3cad97c71f429b6dadf2eca980671704dd9928
Ruta C <130843221+ruta-c@users.noreply.github.com>
2023-12-15 15:21:36 +0200

    Update Dash App.py

--- Dash App.py
+++ Dash App.py
@@ -10,46 +10,122 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-def load_model_from_s3():
+def load_model_from_s3(model_key):
     s3 = boto3.client('s3', aws_access_key_id='key_id', aws_secret_access_key='secret_key')
     bucket_name = 'price-ml-model'
-    model_key = 'model.joblib'
-
+  
     try:
         # Download the model file from S3
-        s3.download_file(bucket_name, model_key, 'model.joblib')
+        s3.download_file(bucket_name, model_key, model_key)
 
         # Load the model
-        model = joblib.load('model.joblib')
+        model = joblib.load(model_key)
         return model
 
     except NoCredentialsError:
         print('Credentials not available')
 
-model_instance = load_model_from_s3()
+model_instance = load_model_from_s3('model.joblib')
+(X_train, y_train) = load_model_from_s3('training_data.joblib')
 
-app.layout = layout
+app.layout = html.Div([
+    html.Div(id='selected-flat-info-store', style={'display': 'none'}),
+    layout
+])
 
-correct_feature_order = ['area', 'rooms', 'floor', 'floors',
-                          'type_Blokinis', 'type_Kita', 'type_Medinis',
-                          'type_Monolitinis', 'type_Mūrinis', 'mounting_Dalinė apdaila',
-                          'mounting_Kita', 'mounting_Neįrengtas', 'mounting_Įrengtas',
-                          'energy_class_A', 'energy_class_A+', 'energy_class_A++',
-                          'energy_class_B', 'energy_class_Lower than B', 'aeroterminis',
-                          'dujinis', 'elektra', 'geoterminis', 'kietu kuru', 'sildymas_kita',
-                          'saulės energija', 'skystu kuru', 'centrinis_sildymas', 'balkonas',
-                          'drabužinė', 'none', 'palepe', 'pirtis', 'rūsys', 'sandėliukas',
-                          'terasa', 'vieta_automobiliui', 'kameros', 'kodine_spyna', 'sargas',
-                          'sarvuotos_durys', 'signalizacija', 'atskiras įėjimas', 'aukcionas',
-                          'aukštos lubos', 'butas palėpėje', 'butas per kelis aukštus',
-                          'buto dalis', 'internetas', 'kabelinė televizija',
-                          'nauja elektros instaliacija', 'nauja kanalizacija',
-                          'tualetas ir vonia atskirai', 'uždaras kiemas',
-                          'virtuvė sujungta su kambariu', 'building_age', 'building_age_reno',
-                          'distance_to_center']
+correct_feature_order = [
+    'area', 'rooms', 'floor', 'floors',
+    'type_Blokinis', 'type_Kita', 'type_Medinis',
+    'type_Monolitinis', 'type_Mūrinis', 'mounting_Dalinė apdaila',
+    'mounting_Kita', 'mounting_Neįrengtas', 'mounting_Įrengtas',
+    'energy_class_A', 'energy_class_A+', 'energy_class_A++',
+    'energy_class_B', 'energy_class_Lower than B', 'aeroterminis',
+    'dujinis', 'elektra', 'geoterminis', 'kietu kuru', 'sildymas_kita',
+    'saulės energija', 'skystu kuru', 'centrinis_sildymas', 'balkonas',
+    'drabužinė', 'none', 'palepe', 'pirtis', 'rūsys', 'sandėliukas',
+    'terasa', 'vieta_automobiliui', 'kameros', 'kodine_spyna', 'sargas',
+    'sarvuotos_durys', 'signalizacija', 'atskiras įėjimas', 'aukcionas',
+    'aukštos lubos', 'butas palėpėje', 'butas per kelis aukštus',
+    'buto dalis', 'internetas', 'kabelinė televizija',
+    'nauja elektros instaliacija', 'nauja kanalizacija',
+    'tualetas ir vonia atskirai', 'uždaras kiemas',
+    'virtuvė sujungta su kambariu', 'building_age', 'building_age_reno',
+    'distance_to_center'
+]
+
+nn_model = NearestNeighbors(n_neighbors=4)
+nn_model.fit(X_train[correct_feature_order])
+
+def process_dropdown_values(feature, selected_value, input_data):
+    if selected_value:
+        if feature == 'other':
+            for value in selected_value:
+                column_name = f'{value}'
+                input_data[column_name] = 1
+                print(f"Feature: {feature}, Selected Value: {value}, Column Name: {column_name}")
+        else:
+            column_name = f'{selected_value}'
+            input_data[column_name] = 1
+            print(f"Feature: {feature}, Selected Value: {selected_value}, Column Name: {column_name}")
+
+def create_input_data(area, rooms, floor, floors, building_age, building_age_reno, distance_to_center,
+                      dropdown_type, dropdown_mounting, dropdown_energy_class, dropdown_heating, dropdown_other):
+    input_data = {
+        'area': area,
+        'rooms': rooms,
+        'floor': floor,
+        'floors': floors,
+        'building_age': building_age,
+        'building_age_reno': building_age_reno,
+        'distance_to_center': distance_to_center,
+    }
+
+    dropdowns = {
+        'type': dropdown_type,
+        'mounting': dropdown_mounting,
+        'energy_class': dropdown_energy_class,
+        'heating': dropdown_heating,
+        'other': dropdown_other
+    }
+
+    for feature, selected_value in dropdowns.items():
+        process_dropdown_values(feature, selected_value, input_data)
+
+    return pd.DataFrame([input_data]).reindex(columns=correct_feature_order, fill_value=0)
+
+def update_similar_flats_df(indices, input_data, label_mapping):
+    similar_flats_df = pd.DataFrame(columns=['Property', 'Value'])
+
+    for i, idx in enumerate(indices[0][1:]):
+        similar_flats_df.loc[len(similar_flats_df)] = {
+            'Property': f"Similar Flat {i + 1} - Predicted Price",
+            'Value': f"{y_train.iloc[idx]:.2f} €/sqm"
+        }
+
+        for col in ['area', 'rooms', 'floor', 'floors', 'building_age', 'building_age_reno', 'distance_to_center']:
+            label = label_mapping.get(col, col)
+
+            if col == 'distance_to_center':
+                value = f"{X_train[col].iloc[idx]:.2f} km"
+            else:
+                value = X_train[col].iloc[idx]
+
+            similar_flats_df.loc[len(similar_flats_df)] = {'Property': label, 'Value': value}
+
+        for col in X_train.columns:
+            if col != 'rooms' and X_train[col].iloc[idx] == 1:
+                label_display = label_mapping.get(col, col)
+                similar_flats_df.loc[len(similar_flats_df)] = {'Property': label_display, 'Value': 'Yes'}
+
+    return similar_flats_df
 
 @app.callback(
-    Output('predicted-price-output', 'children'),
+    [
+        Output('predicted-price-output', 'children'),
+        Output('similar-flats-table-1', 'data'),
+        Output('similar-flats-table-2', 'data'),
+        Output('similar-flats-table-3', 'data'),
+    ],
     [Input('predict-button', 'n_clicks')],
     [
         State('area', 'value'),
@@ -59,46 +135,117 @@ correct_feature_order = ['area', 'rooms', 'floor', 'floors',
         State('building_age', 'value'),
         State('building_age_reno', 'value'),
         State('distance_to_center', 'value'),
-        *[State(f'checklist-col-{i}', 'value') for i in range(num_cols)]
+        State('dropdown-type', 'value'),
+        State('dropdown-mounting', 'value'),
+        State('dropdown-energy-class', 'value'),
+        State('dropdown-heating', 'value'),
+        State('dropdown-other', 'value'),
     ]
 )
+
 def update_predicted_price(n_clicks, area, rooms, floor, floors, building_age, building_age_reno, distance_to_center,
-                           *selected_features):
-    # Check if the button was clicked
+                           dropdown_type, dropdown_mounting, dropdown_energy_class, dropdown_heating, dropdown_other):
     if n_clicks and n_clicks > 0:
-        if None not in [area, rooms, floor, floors, building_age, building_age_reno, distance_to_center]:
-            selected_features_flat = [item for sublist in selected_features for item in sublist]
-            selected_features_set = set(selected_features_flat)
-            selected_features_dict = {feature: 1 if feature in selected_features_set else 0 for feature in
-                                      correct_feature_order}
+        if all(v is not None and v >= 0 for v in [area, rooms, floor, floors, building_age, building_age_reno, distance_to_center]):
+            if floor <= floors and building_age >= building_age_reno:
+                input_data = create_input_data(area, rooms, floor, floors, building_age, building_age_reno, distance_to_center,
+                                               dropdown_type, dropdown_mounting, dropdown_energy_class, dropdown_heating, dropdown_other)
 
-            # Create a dictionary to hold input values and selected categorical features
-            numeric_data = {
-                'area': area,
-                'rooms': rooms,
-                'floor': floor,
-                'floors': floors,
-                'building_age': building_age,
-                'building_age_reno': building_age_reno,
-                'distance_to_center': distance_to_center
-            }
-            # Update the input_data dictionary with selected_features_dict
-            input_data = selected_features_dict.copy()  
-            input_data.update(numeric_data)  
-            input_df = pd.DataFrame([input_data])
-            input_df = input_df[correct_feature_order]
+                predicted_price = model_instance.predict_price(input_data)[0]
+                _, indices = nn_model.kneighbors(input_data)
 
-            # Use the pretrained model to predict the price
-            predicted_price = model_instance.predict_price(input_df)[0]
+                label_mapping = {
+                    'area': 'Area (sqm)',
+                    'rooms': 'Number of Rooms',
+                    'floor': 'Floor',
+                    'floors': 'Number of Floors',
+                    'building_age': 'Building Age (years)',
+                    'building_age_reno': 'Building Renovation Age (years)',
+                    'distance_to_center': 'Distance to Center (km)',
+                    'type_Blokinis': 'Concrete Block Construction',
+                    'energy_class_A++': 'Energy Class A++',
+                    'energy_class_A': 'Energy Class A',
+                    'aeroterminis': 'Aerothermal Heating',
+                    'type_Medinis': 'Wooden Construction',
+                    'energy_class_A+': 'Energy Class A+',
+                    'dujinis': 'Gas Heating',
+                    'type_Monolitinis': 'Monolithic Construction',
+                    'energy_class_B': 'Energy Class B',
+                    'elektra': 'Electric Heating',
+                    'type_Mūrinis': 'Brick Construction',
+                    'energy_class_Lower than B': 'Energy Class Lower Than B',
+                    'geoterminis': 'Geothermal Heating',
+                    'type_Kita': 'Other Construction',
+                    'energy_class_Not specified': 'Energy Class Not Specified',
+                    'kietu kuru': 'Solid Fuel Heating',
+                    'mounting_Įrengtas': 'Fully Furnished',
+                    'balkonas': 'Balcony',
+                    'centrinis_sildymas': 'Central Heating',
+                    'mounting_Dalinė apdaila': 'Partly Furnished',
+                    'drabužinė': 'Closet',
+                    'saulės energija': 'Solar Power Heating',
+                    'mounting_Neįrengtas': 'Not Furnished',
+                    'palepe': 'Attic',
+                    'skystu kuru': 'Liquid Fuel Heating',
+                    'mounting_Kita': 'Other Furnishing',
+                    'pirtis': 'Sauna',
+                    'sildymas_kita': 'Other Heating',
+                    'rūsys': 'Basement',
+                    'sandėliukas': 'Pantry',
+                    'terasa': 'Terrace',
+                    'vieta_automobiliui': 'Parking Space',
+                    'kameros': 'Security Cameras',
+                    'kodine_spyna': 'Combination Lock Door',
+                    'sargas': 'Security Guard',
+                    'sarvuotos_durys': 'Steel Doors',
+                    'signalizacija': 'Alarm System',
+                    'atskiras įėjimas': 'Separate Entrance',
+                    'aukcionas': 'Auction',
+                    'aukštos lubos': 'High Ceilings',
+                    'butas palėpėje': 'Attic Apartment',
+                    'butas per kelis aukštus': 'Multi-Floor Apartment',
+                    'buto dalis': 'Part of the Apartment',
+                    'internetas': 'Internet Connection',
+                    'kabelinė televizija': 'Cable TV',
+                    'nauja elektros instaliacija': 'New Electrical Installation',
+                    'nauja kanalizacija': 'New Plumbing',
+                    'tualetas ir vonia atskirai': 'Separate Toilet and Bathroom',
+                    'uždaras kiemas': 'Closed Courtyard',
+                    'virtuvė sujungta su kambariu': 'Kitchen Connected with Room',
+                    'none': 'No Special Properties'
+                }
+                predicted_price_output = "Predicted Price: {:.2f} €/sqm".format(predicted_price)
 
-            return f'Predicted Price: {predicted_price:.2f} €/sqm'
+                similar_flats_dfs = [pd.DataFrame(columns=['Property', 'Value']) for _ in range(3)]
 
+                for i, idx in enumerate(indices[0][1:4]):  # Consider only the first three similar flats
+                    similar_flats_dfs[i].loc[len(similar_flats_dfs[i])] = {
+                        'Property': f"Price, €/sqm",
+                        'Value': f"{y_train.iloc[idx]:.2f}"
+                    }
+
+                    for col in ['area', 'rooms', 'floor', 'floors', 'building_age', 'building_age_reno', 'distance_to_center']:
+                        label = label_mapping.get(col, col)
+
+                        if col == 'distance_to_center':
+                            value = f"{X_train[col].iloc[idx]:.2f}"
+                        else:
+                            value = X_train[col].iloc[idx]
+
+                        similar_flats_dfs[i].loc[len(similar_flats_dfs[i])] = {'Property': label, 'Value': value}
+
+                    for col in X_train.columns:
+                        if col != 'rooms' and X_train[col].iloc[idx] == 1:
+                            label_display = label_mapping.get(col, col)
+                            similar_flats_dfs[i].loc[len(similar_flats_dfs[i])] = {'Property': label_display, 'Value': 'Yes'}
+
+                return predicted_price_output, similar_flats_dfs[0].to_dict('records'), similar_flats_dfs[1].to_dict('records'), similar_flats_dfs[2].to_dict('records')
+            else:
+                return "Please fill out all input fields. Floor should be less than or equal to total floors, and building age should be greater than renovated building age.", [], [], []
         else:
-            return "Please fill out all input fields."
+            return "Error: Please fill out all input fields with positive values.", [], [], []
     else:
-        # If the button was not clicked, return an empty string
-        return ''
+        return '', [], [], []
 
-#port = int(os.environ.get("PORT", 8050))
 if __name__ == '__main__':
     app.run_server(debug=True)
