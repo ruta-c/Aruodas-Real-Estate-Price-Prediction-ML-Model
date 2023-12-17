32a38335efff6c37ce13da64cd8804be4d9621f9
Ruta C <130843221+ruta-c@users.noreply.github.com>
2023-12-17 21:55:10 +0200

    Update Aruodas Scrapper.py

--- Aruodas Scrapper.py
+++ Aruodas Scrapper.py
@@ -17,7 +17,6 @@ def get_advert_data(building_type, where):
             url = base_url.format(page)
             driver.get(url)
             
-            # Handle cookies banner
             try:
                 cookie_banner = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.ID, 'onetrust-reject-all-handler'))
@@ -26,7 +25,6 @@ def get_advert_data(building_type, where):
             except:
                 pass
             
-            # Find all advertisement links using XPath
             advert_links = [link.get_attribute('href') for link in driver.find_elements(By.XPATH, '//h3/a[@href]')]
             
             if not advert_links:
@@ -35,12 +33,10 @@ def get_advert_data(building_type, where):
             for link in advert_links:
                 driver.get(link)
                 data_dict = {}
-                # Find the element with the 'price-eur' class
                 price_element = driver.find_element(By.CLASS_NAME, 'price-eur')
                 price_value = price_element.text
                 data_dict['Price (EUR)'] = price_value
                 
-                # Find the <a> element containing the coordinates (with explicit wait)
                 try:
                     wait = WebDriverWait(driver, 10)
                     a_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[data-type='map']")))
@@ -54,7 +50,6 @@ def get_advert_data(building_type, where):
                 except:
                     pass
                 
-                # Find all <dt> and <dd> elements containing property information
                 dt_elements = driver.find_elements(By.TAG_NAME, 'dt')
                 dd_elements = driver.find_elements(By.TAG_NAME, 'dd')
                 
@@ -76,17 +71,11 @@ def get_advert_data(building_type, where):
 flats_df = get_advert_data('butai', 'vilniuje')
 
 engine = create_engine('postgresql://xxxxxx:yyyyyyy@qqqqqqqqqqq.rds.amazonaws.com:0000/db_name')
-
-# Load existing data from the database
 existing_data_df = pd.read_sql('SELECT * FROM uncleaned_flats', con=engine)
 
-# Concatenate existing and new data, dropping duplicates based on all columns except 'price' and 'Nuoroda'
 combined_df = pd.concat([existing_data_df, flats_df], ignore_index=True)
 unique_data_df = combined_df.drop_duplicates(subset=combined_df.columns.difference(['Price (EUR)', 'Nuoroda']), keep='last')
 rows_to_insert = len(unique_data_df)
 
-# Insert unique data into the database
 unique_data_df.to_sql('uncleaned_flats', con=engine, if_exists='replace', index=False)
-
-# Print the number of rows inserted
 print(f"Number of rows: {rows_to_insert}")
