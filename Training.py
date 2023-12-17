a0176054e8b5ac2bfcfc165c7bcf9c2543bc0fb5
Ruta C <130843221+ruta-c@users.noreply.github.com>
2023-12-17 21:53:55 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -6,6 +6,4 @@ X_train, y_train = model.get_training_data()
 joblib.dump((X_train, y_train), 'training_data.joblib')
 scatter_plot = model.get_scatter_plot()
 scatter_json = scatter_plot.to_json()
-
-# Use joblib for model serialization
 joblib.dump(model, 'model.joblib')
